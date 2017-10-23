abc938abf0bd7d9ae89c37159428bf4fce6516bc
Viktor Holtenäs <viktor@ustwo.com>
2017-10-23 16:00:02 +0200

    Added Prettier
    Formatted some code.

--- index.android.js
+++ index.android.js
@@ -3,133 +3,125 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import React, {Component} from "react";
-import Nav from "./src/components/android/navigation/NavAndroid";
-import store from "./src/store/configureStore";
-import {Provider} from "react-redux";
-import {loadGuides} from "./src/actions/guideActions";
-import {loadSubLocations} from "./src/actions/subLoactionActions";
-import {internetChanged} from "./src/actions/internetActions";
-import {AppRegistry , Alert, StyleSheet, NetInfo, UIManager, AsyncStorage} from "react-native";
-import {LangService} from "./src/services/langService";
+import React, { Component } from 'react';
+import Nav from './src/components/android/navigation/NavAndroid';
+import store from './src/store/configureStore';
+import { Provider } from 'react-redux';
+import { loadGuides } from './src/actions/guideActions';
+import { loadSubLocations } from './src/actions/subLoactionActions';
+import { internetChanged } from './src/actions/internetActions';
+import { AppRegistry, Alert, StyleSheet, NetInfo, UIManager, AsyncStorage } from 'react-native';
+import { LangService } from './src/services/langService';
 import Opener from './src/services/SettingsService';
-import {errorHappened} from "./src/actions/errorActions";
-import { getStoredState} from 'redux-persist'
-import {DownloadTasksManager} from './src/services/DownloadTasksManager';
-
+import { errorHappened } from './src/actions/errorActions';
+import { getStoredState } from 'redux-persist';
+import { DownloadTasksManager } from './src/services/DownloadTasksManager';
 
 export default class GuideHbg extends Component {
+  noNetworkTimer;
+  downloadManager;
 
+  constructor() {
+    super();
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
 
-    noNetworkTimer;
-    downloadManager;
+    this.init = this.init.bind(this);
+    this.handleConnectivityChange = this.handleConnectivityChange.bind(this);
 
+    this.downloadManager = DownloadTasksManager.getInstance();
 
-    constructor() {
-        super();
-        UIManager.setLayoutAnimationEnabledExperimental &&
-        UIManager.setLayoutAnimationEnabledExperimental(true);
+    console.ignoredYellowBox = ['Debug Console'];
+  }
 
-        this.init = this.init.bind(this);
-        this.handleConnectivityChange = this.handleConnectivityChange.bind(this);
+  componentDidMount() {
+    //this.init();
+    LangService.loadStoredLanguage();
+    this.startListeningToNetworkChanges();
+    this.loadExistingDownloads();
+  }
 
-        this.downloadManager = DownloadTasksManager.getInstance();
+  componentWillUnmount() {
+    console.log('app will unmounted');
+    this.stopListeningToNetworkChanges();
+  }
 
+  init() {
+    LangService.loadStoredLanguage()
+      .then(loaded => {
+        //Check the network and load the content.
+        this.loadContents(LangService.code);
+      })
+      .catch(error => store.dispatch(errorHappened(error)));
+  }
+
+  //####################################################
+  // method on app load-
+  loadExistingDownloads() {
+    getStoredState({ storage: AsyncStorage }, (err, state) => {
+      if (state && state.downloads && state.downloads.length)
+        this.downloadManager.loadExistingTasks(state.downloads);
+    });
+  }
+  //########################################################
+
+  alert() {
+    console.log('I will alert##################');
+    Alert.alert(
+      LangService.strings.NO_INTERNET_CONNECTION,
+      LangService.strings.NO_INTERNET_CONNECTION_MESSAGE,
+      [
+        { text: LangService.strings.SETTINGS, onPress: this.openInternetSettings.bind(this) },
+        { text: LangService.strings.CLOSE, onPress: () => {}, style: 'cancel' },
+      ],
+      { cancelable: false },
+    );
+  }
+
+  openInternetSettings() {
+    Opener.openWifiSetting();
+  }
+
+  startListeningToNetworkChanges() {
+    NetInfo.isConnected.addEventListener('change', this.handleConnectivityChange);
+  }
+  stopListeningToNetworkChanges() {
+    NetInfo.isConnected.removeEventListener('change', this.handleConnectivityChange);
+  }
+  handleConnectivityChange(isConnected) {
+    if (!isConnected) {
+      store.dispatch(internetChanged(false));
+      this.noNetworkTimer = setTimeout(() => this.alert(), 2500);
+      return;
+    }
+    console.log('There is connection');
+    store.dispatch(internetChanged(true));
+
+    if (this.noNetworkTimer) {
+      clearTimeout(this.noNetworkTimer);
+      this.noNetworkTimer = null;
     }
 
-    componentDidMount(){
-        //this.init();
-        LangService.loadStoredLanguage();
-        this.startListeningToNetworkChanges();
-        this.loadExistingDownloads();
-    }
+    this.init();
+  }
 
-    componentWillUnmount(){
-        console.log('app will unmounted');
-        this.stopListeningToNetworkChanges();
-    }
+  loadContents(langCode) {
+    NetInfo.isConnected.fetch().then(isConnected => {
+      if (isConnected) {
+        store.dispatch(loadGuides(langCode));
+        store.dispatch(loadSubLocations(langCode));
+        LangService.getLanguages().catch(error => console.log('error in getting lang'));
+      }
+    });
+  }
 
-    init(){
-        LangService.loadStoredLanguage()
-            .then(loaded=>{
-            //Check the network and load the content.
-            this.loadContents(LangService.code);
-        }).catch(error=>store.dispatch(errorHappened(error)));
-    }
-
-    //####################################################
-    // method on app load-
-    loadExistingDownloads(){
-        getStoredState({storage: AsyncStorage},((err,state)=> {
-            if (state && state.downloads&& state.downloads.length)
-                this.downloadManager.loadExistingTasks(state.downloads)
-        }));
-    }
-//########################################################
-
-    alert(){
-        console.log('I will alert##################');
-        Alert.alert(
-            LangService.strings.NO_INTERNET_CONNECTION,
-            LangService.strings.NO_INTERNET_CONNECTION_MESSAGE,
-            [
-                {text: LangService.strings.SETTINGS, onPress: this.openInternetSettings.bind(this)},
-                {text: LangService.strings.CLOSE, onPress: () => {}, style: 'cancel'},
-            ],
-            { cancelable: false }
-        )
-    }
-
-    openInternetSettings(){
-        Opener.openWifiSetting();
-    }
-
-    startListeningToNetworkChanges(){
-        NetInfo.isConnected.addEventListener(
-            'change',
-            this.handleConnectivityChange
-        );
-    }
-    stopListeningToNetworkChanges(){
-        NetInfo.isConnected.removeEventListener(
-            'change',
-            this.handleConnectivityChange
-        );
-    }
-    handleConnectivityChange(isConnected) {
-        if(!isConnected){
-            store.dispatch(internetChanged(false));
-            this.noNetworkTimer= setTimeout(()=>this.alert(),2500);
-            return;
-        }
-        console.log('There is connection');
-        store.dispatch(internetChanged(true));
-
-        if(this.noNetworkTimer){
-            clearTimeout(this.noNetworkTimer);
-            this.noNetworkTimer = null;
-        }
-
-        this.init();
-    }
-
-    loadContents(langCode){
-        NetInfo.isConnected.fetch().then(isConnected=>{
-            if(isConnected){
-                store.dispatch(loadGuides(langCode));
-                store.dispatch(loadSubLocations(langCode));
-                LangService.getLanguages().catch(error=>console.log('error in getting lang'));
-            }
-        });
-    }
-
-    render() {
-        return (
-            <Provider store={store}>
-                <Nav />
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Nav />
+      </Provider>
+    );
+  }
 }
 
 AppRegistry.registerComponent('GuideHbg', () => GuideHbg);
--- index.ios.js
+++ index.ios.js
@@ -5,17 +5,16 @@
  */
 
 import React, { Component } from 'react';
-import Nav from './src/components/android/navigation/NavAndroid'
+import Nav from './src/components/android/navigation/NavAndroid';
 import store from './src/store/configureStore';
-import {Provider} from 'react-redux';
-import {loadGuides} from "./src/actions/guideActions";
-import {loadSubLocations} from "./src/actions/subLoactionActions";
-import {getStoredState} from 'redux-persist'
-import {LangService} from "./src/services/langService";
-import {DownloadTasksManager} from './src/services/DownloadTasksManager';
-import {internetChanged} from "./src/actions/internetActions";
-import {errorHappened} from "./src/actions/errorActions";
-
+import { Provider } from 'react-redux';
+import { loadGuides } from './src/actions/guideActions';
+import { loadSubLocations } from './src/actions/subLoactionActions';
+import { getStoredState } from 'redux-persist';
+import { LangService } from './src/services/langService';
+import { DownloadTasksManager } from './src/services/DownloadTasksManager';
+import { internetChanged } from './src/actions/internetActions';
+import { errorHappened } from './src/actions/errorActions';
 
 import {
   AppRegistry,
@@ -24,120 +23,104 @@ import {
   View,
   PushNotificationIOS,
   AlertIOS,
-    NetInfo,
-    UIManager,
-    AsyncStorage,
-    Linking
-
+  NetInfo,
+  UIManager,
+  AsyncStorage,
+  Linking,
 } from 'react-native';
 
-
 export default class GuideHbg extends Component {
-
-    noNetworkTimer;
-    downloadManager;
+  noNetworkTimer;
+  downloadManager;
 
   constructor() {
-  super();
-        UIManager.setLayoutAnimationEnabledExperimental &&
-        UIManager.setLayoutAnimationEnabledExperimental(true);
+    super();
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
 
-        this.init = this.init.bind(this);
-        this.handleConnectivityChange = this.handleConnectivityChange.bind(this);
+    this.init = this.init.bind(this);
+    this.handleConnectivityChange = this.handleConnectivityChange.bind(this);
 
-        this.downloadManager = DownloadTasksManager.getInstance();
-
+    this.downloadManager = DownloadTasksManager.getInstance();
   }
 
-  componentDidMount(){
-   
-        LangService.loadStoredLanguage();
-        this.startListeningToNetworkChanges();
-        this.loadExistingDownloads();
+  componentDidMount() {
+    LangService.loadStoredLanguage();
+    this.startListeningToNetworkChanges();
+    this.loadExistingDownloads();
+  }
+
+  componentWillUnmount() {
+    this.stopListeningToNetworkChanges();
+  }
+
+  init() {
+    LangService.loadStoredLanguage()
+      .then(loaded => {
+        //Check the network and load the content.
+        this.loadContents(LangService.code);
+      })
+      .catch(error => store.dispatch(errorHappened(error)));
+  }
+  loadExistingDownloads() {
+    getStoredState({ storage: AsyncStorage }, (err, state) => {
+      if (!err && state && state.downloads && state.downloads.length)
+        this.downloadManager.loadExistingTasks(state.downloads);
+    });
+  }
+  alert() {
+    AlertIOS.alert(
+      LangService.strings.NO_INTERNET_CONNECTION,
+      LangService.strings.NO_INTERNET_CONNECTION_MESSAGE,
+      [
+        { text: LangService.strings.SETTINGS, onPress: this.openInternetSettings.bind(this) },
+        { text: LangService.strings.CLOSE, onPress: () => {}, style: 'cancel' },
+      ],
+    );
+  }
+  openInternetSettings() {
+    Linking.openURL('app-settings:');
+  }
+  startListeningToNetworkChanges() {
+    NetInfo.isConnected.addEventListener('change', this.handleConnectivityChange);
+  }
+  stopListeningToNetworkChanges() {
+    NetInfo.isConnected.removeEventListener('change', this.handleConnectivityChange);
+  }
+  handleConnectivityChange(isConnected) {
+    if (!isConnected) {
+      store.dispatch(internetChanged(false));
+      this.noNetworkTimer = setTimeout(() => this.alert(), 2500);
+      return;
     }
 
-    componentWillUnmount(){
-      
-        this.stopListeningToNetworkChanges();
+    store.dispatch(internetChanged(true));
+
+    if (this.noNetworkTimer) {
+      clearTimeout(this.noNetworkTimer);
+      this.noNetworkTimer = null;
     }
 
-      init(){
-        LangService.loadStoredLanguage()
-            .then(loaded=>{
-            //Check the network and load the content.
-            this.loadContents(LangService.code);
-        }).catch(error=>store.dispatch(errorHappened(error)));
-    }
-    loadExistingDownloads(){
-        getStoredState({storage: AsyncStorage},((err,state)=> {
+    this.init();
+  }
 
-            if (!err &&state && state.downloads&& state.downloads.length)
-                this.downloadManager.loadExistingTasks(state.downloads)
-        }));
-    }
-    alert(){
-        
-        AlertIOS.alert(
-            LangService.strings.NO_INTERNET_CONNECTION,
-            LangService.strings.NO_INTERNET_CONNECTION_MESSAGE,
-            [
-                {text: LangService.strings.SETTINGS, onPress: this.openInternetSettings.bind(this)},
-                {text: LangService.strings.CLOSE, onPress: () => {}, style: 'cancel'},
-            ]
-        )
-    }
-    openInternetSettings(){
-        Linking.openURL('app-settings:');
-    }
-    startListeningToNetworkChanges(){
-        NetInfo.isConnected.addEventListener(
-            'change',
-            this.handleConnectivityChange
-        );
-    }
-    stopListeningToNetworkChanges(){
-        NetInfo.isConnected.removeEventListener(
-            'change',
-            this.handleConnectivityChange
-        );
-    }
-    handleConnectivityChange(isConnected) {
-        if(!isConnected){
-            store.dispatch(internetChanged(false));
-            this.noNetworkTimer= setTimeout(()=>this.alert(),2500);
-            return;
-        }
-   
-        store.dispatch(internetChanged(true));
-
-        if(this.noNetworkTimer){
-            clearTimeout(this.noNetworkTimer);
-            this.noNetworkTimer = null;
-        }
-
-        this.init();
-    }
-
-    loadContents(langCode){
-        NetInfo.isConnected.fetch().then(isConnected=>{
-            if(isConnected){
-                store.dispatch(loadGuides(langCode));
-                store.dispatch(loadSubLocations(langCode));
-                LangService.getLanguages().catch(error=>console.log('error in getting lang'));
-            }
-        });
-    }
-
+  loadContents(langCode) {
+    NetInfo.isConnected.fetch().then(isConnected => {
+      if (isConnected) {
+        store.dispatch(loadGuides(langCode));
+        store.dispatch(loadSubLocations(langCode));
+        LangService.getLanguages().catch(error => console.log('error in getting lang'));
+      }
+    });
+  }
 
   render() {
     return (
       <Provider store={store}>
-      <Nav reloadFunc={this.loadContents}/>
+        <Nav reloadFunc={this.loadContents} />
       </Provider>
     );
   }
-
-
 }
 
 const styles = StyleSheet.create({
