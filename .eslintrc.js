600032402806181b0059364883e5c72df52c4ce7
Viktor Holtenäs <viktor@ustwo.com>
2017-10-24 13:39:27 +0200

    Updated eslint template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,39 @@
 module.exports = {
-  extends: 'airbnb',
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: [],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'local',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        varsIgnorePattern: '^unused',
+        argsIgnorePattern: '^unused',
+      },
+    ],
+    'max-len': [
+      'error',
+      140,
+      {
+        ignoreComments: true,
+        ignoreUrls: true,
+      },
+    ],
+    'no-console': ['off'],
   },
 };
