5ca1614fa5e7b134fd0f011cf96e076f2cf3eba5
Viktor Holtenäs <viktor@ustwo.com>
2017-10-30 10:37:42 +0100

    Formatted code

--- src/components/scenes/WelcomeView.js
+++ src/components/scenes/WelcomeView.js
@@ -4,185 +4,181 @@
 /**
  * Created by msaeed on 2017-02-04.
  */
-import React, {Component} from 'react';
-import {View,Text, StyleSheet, ScrollView, Button, Dimensions, Image, TouchableOpacity,  AsyncStorage} from 'react-native';
-import ViewContainer from '../shared/view_container'
-import FirstInstructionSlide from '../shared/FirstInstructionSlide'
-import NormalInstructionSlide from '../shared/NormalInstructionSlide'
-import Swiper from 'react-native-swiper';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, ScrollView, Button, Dimensions, Image, TouchableOpacity, AsyncStorage } from "react-native";
+import ViewContainer from "../shared/view_container";
+import FirstInstructionSlide from "../shared/FirstInstructionSlide";
+import NormalInstructionSlide from "../shared/NormalInstructionSlide";
+import Swiper from "react-native-swiper";
 import GuideList from "./GuideList";
-import {IS_WELCOMED} from '../../lib/my_consts';
-import {LangService} from "../../services/langService";
+import { IS_WELCOMED } from "../../lib/my_consts";
+import { LangService } from "../../services/langService";
 import ColoredBar from "../shared/ColoredBar";
 
+const STYLES = [{ backgroundColor: "#7B075E" }, { backgroundColor: "#A61380" }, { backgroundColor: "#c44f8a" }];
 
-const STYLES = [
-    {backgroundColor: '#7B075E'},
-    {backgroundColor: '#A61380'},
-    {backgroundColor: '#c44f8a'},
+const IMAGE1 = require("../../images/firstInstructionImage.png");
 
-];
+const FULL_HEIGHT = Dimensions.get("window").height;
 
-const IMAGE1 = require('../../images/firstInstructionImage.png');
-const FULL_HEIGHT = Dimensions.get('window').height;
+export default class WelcomeView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      total: 0,
+    };
+    this._onMomentumScrollEnd = this._onMomentumScrollEnd.bind(this);
+  }
 
-export default class WelcomeView extends Component{
+  componentDidMount() {}
 
-    constructor(props){
-        super(props);
-        this.state = {
-            index:0,
-            total:0
-        };
-        this._onMomentumScrollEnd=this. _onMomentumScrollEnd.bind(this);
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.index == 0 || nextState.index == 1 || nextState.index == nextState.total - 1 || nextState.index == nextState.total - 2) {
+      return true;
     }
 
-    componentDidMount(){
-    }
+    return false;
+  }
 
-    shouldComponentUpdate(nextProps, nextState){
-        if(nextState.index ==0 || nextState.index==1||nextState.index==nextState.total-1 || nextState.index== nextState.total-2)
-            return true;
+  skipPress() {
+    const mainRoute = {
+      title: "GuidesList",
+      type: "fade",
+      component: GuideList,
+    };
+    AsyncStorage.setItem(IS_WELCOMED, JSON.stringify(true));
+    this.props.navigator.resetTo(mainRoute);
+  }
 
-        return false;
-    }
-
-    skipPress() {
-        const mainRoute = {
-            title: 'GuidesList',
-            type:'fade',
-            component:GuideList
-        };
-        AsyncStorage.setItem(IS_WELCOMED,JSON.stringify(true));
-        this.props.navigator.resetTo(mainRoute);
-    }
-
-
-    displaySlides(){
-        let instructions = LangService.instructions.steps;
-        return instructions.map((item,index)=>{
-            let btnText = (index==instructions.length-1)?LangService.strings.LETS_GO:LangService.strings.JUMP_OVER;
-            if(index==0)
-            return (<FirstInstructionSlide key={index} styler={STYLES[index]}
-                                           content={item.content}
-                                           closeBtnText={btnText}
-                                           backgroundImageSource={IMAGE1}
-            />);
-
-            return (<NormalInstructionSlide key={index} styler={STYLES[index]}
-                                           content={item.content}
-                                           thumbnailSource={item.thumbnail}
-                                           closeBtnText={btnText}
-                                            onBtnPress={()=>this.skipPress()}
-                                           backgroundImageSource={item.background}
-            />);
-
-        });
-    }
-
-    _onMomentumScrollEnd(e,state,context){
-        this.setState({index:context.state.index, total:context.state.total});
-    }
-
-    displaySkipBtn(){
-
-        if(this.state.index==0)
-            return <View></View>;
-       
+  displaySlides() {
+    const instructions = LangService.instructions.steps;
+    return instructions.map((item, index) => {
+      const btnText = index == instructions.length - 1 ? LangService.strings.LETS_GO : LangService.strings.JUMP_OVER;
+      if (index == 0) {
         return (
-            <View  style={styles.btnContainer}>
-                <TouchableOpacity onPress ={()=>this.skipPress()}>
-                    <Text style={styles.btnText}>
-                        {(this.state.index==this.state.total-1)?LangService.strings.LETS_GO:LangService.strings.JUMP_OVER}
-                    </Text>
-                </TouchableOpacity>
-            </View>
+          <FirstInstructionSlide
+            key={index}
+            styler={STYLES[index]}
+            content={item.content}
+            closeBtnText={btnText}
+            backgroundImageSource={IMAGE1}
+          />
         );
-    }
+      }
 
-    displayColorBar(){
-        return(
-            <View style={{position:'absolute', height:FULL_HEIGHT,width:20, top:0,left:0}}>
-                <ColoredBar visible={true}/>
-            </View>
-        );
-    }
+      return (
+        <NormalInstructionSlide
+          key={index}
+          styler={STYLES[index]}
+          content={item.content}
+          thumbnailSource={item.thumbnail}
+          closeBtnText={btnText}
+          onBtnPress={() => this.skipPress()}
+          backgroundImageSource={item.background}
+        />
+      );
+    });
+  }
 
-    render(){
+  _onMomentumScrollEnd(e, state, context) {
+    this.setState({ index: context.state.index, total: context.state.total });
+  }
 
-        const dot = (<View style={styles.dot} /> );
-        const activeDot = (<View style={styles.activeDot} />);
+  displaySkipBtn() {
+    if (this.state.index == 0) return <View />;
 
-        return (
-             <ViewContainer style={[styles.wrapper]} >
+    return (
+      <View style={styles.btnContainer}>
+        <TouchableOpacity onPress={() => this.skipPress()}>
+          <Text style={styles.btnText}>
+            {this.state.index == this.state.total - 1 ? LangService.strings.LETS_GO : LangService.strings.JUMP_OVER}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
-                 <Swiper style={styles.swiper}
-                         onMomentumScrollEnd ={this._onMomentumScrollEnd}
-                         loop={false}
-                         dot = {dot}
-                         paginationStyle={styles.pagination}
-                         activeDot = {activeDot}
-                         showsButtons={false}>
-                     {this.displaySlides()}
+  displayColorBar() {
+    return (
+      <View style={{ position: "absolute", height: FULL_HEIGHT, width: 20, top: 0, left: 0 }}>
+        <ColoredBar visible />
+      </View>
+    );
+  }
 
-                 </Swiper>
-                 {this.displaySkipBtn()}
-                 {this.displayColorBar()}
+  render() {
+    const dot = <View style={styles.dot} />;
+    const activeDot = <View style={styles.activeDot} />;
 
-             </ViewContainer>
-        );
-    }
-
+    return (
+      <ViewContainer style={[styles.wrapper]}>
+        <Swiper
+          style={styles.swiper}
+          onMomentumScrollEnd={this._onMomentumScrollEnd}
+          loop={false}
+          dot={dot}
+          paginationStyle={styles.pagination}
+          activeDot={activeDot}
+          showsButtons={false}
+        >
+          {this.displaySlides()}
+        </Swiper>
+        {this.displaySkipBtn()}
+        {this.displayColorBar()}
+      </ViewContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        flex:1,
-    },
-    swiper: {
-        flex:1,
-        backgroundColor: '#7B075E',
-
-    },
-    dot:{backgroundColor:'white',
-        width: 12,
-        height: 12,
-        borderRadius: 6,
-        marginLeft: 10,
-        marginRight: 10,
-        marginTop: 6,
-        marginBottom: 6
-    },
-    activeDot:{backgroundColor: '#D35098',
-        width: 14,
-        height: 14,
-        borderRadius: 7,
-        marginLeft: 11,
-        marginRight:11,
-        marginTop: 7,
-        marginBottom: 7
-    },
-    pagination:{
-        top:300
-    },
-    btnContainer:{
-        flex:1,
-        position:'absolute',
-        bottom:Dimensions.get('window').width*(.2),
-        width:Dimensions.get('window').width,
-        left:0,
-        //paddingVertical:20,
-        //backgroundColor:'red',
-        backgroundColor:'rgba(0,0,0,0)',
-        alignItems:'center',
-        justifyContent:'center',
-    },
-    btnText:{
-        color: '#fff',
-        fontSize: 14,
-        lineHeight:23,
-        borderBottomWidth:1,
-        borderBottomColor:'white'
-    },
-
+  wrapper: {
+    flex: 1,
+  },
+  swiper: {
+    flex: 1,
+    backgroundColor: "#7B075E",
+  },
+  dot: {
+    backgroundColor: "white",
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 6,
+    marginBottom: 6,
+  },
+  activeDot: {
+    backgroundColor: "#D35098",
+    width: 14,
+    height: 14,
+    borderRadius: 7,
+    marginLeft: 11,
+    marginRight: 11,
+    marginTop: 7,
+    marginBottom: 7,
+  },
+  pagination: {
+    top: 300,
+  },
+  btnContainer: {
+    flex: 1,
+    position: "absolute",
+    bottom: Dimensions.get("window").width * 0.2,
+    width: Dimensions.get("window").width,
+    left: 0,
+    // paddingVertical:20,
+    // backgroundColor:'red',
+    backgroundColor: "rgba(0,0,0,0)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 14,
+    lineHeight: 23,
+    borderBottomWidth: 1,
+    borderBottomColor: "white",
+  },
 });
