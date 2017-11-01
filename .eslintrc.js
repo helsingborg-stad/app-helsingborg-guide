a3d0739b8dfe51847989ec35b4699b94bbd1d33f
Viktor Holtenäs <viktor@ustwo.com>
2017-11-01 13:50:56 +0100

    Only warn for prop types

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,28 @@ module.exports = {
     browser: true,
     commonjs: true,
     es6: true,
-    "jest/globals": true
+    "jest/globals": true,
   },
   extends: ["airbnb"],
   globals: {
-    __DEV__: [true, true]
+    __DEV__: [true, true],
   },
   parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 8,
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
-      jsx: true
+      jsx: true,
     },
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["jest", "react"],
   rules: {
-    "quotes": [1, "double"],
+    quotes: [1, "double"],
     "function-paren-newline": ["off"],
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "react/forbid-prop-types": [0],
+    "react/forbid-prop-types": ["warn"],
+    "react/prop-types": [1],
     "no-unused-vars": [
       "error",
       {
@@ -31,25 +32,25 @@ module.exports = {
         args: "after-used",
         ignoreRestSiblings: true,
         varsIgnorePattern: "^unused",
-        argsIgnorePattern: "^unused"
-      }
+        argsIgnorePattern: "^unused",
+      },
     ],
     "max-len": [
       "error",
       140,
       {
         ignoreComments: true,
-        ignoreUrls: true
-      }
+        ignoreUrls: true,
+      },
     ],
     "object-curly-newline": ["error", { consistent: true }],
-    "react/jsx-closing-tag-location": ["off"]
+    "react/jsx-closing-tag-location": ["off"],
   },
   settings: {
     "import/resolver": {
       node: {
-        extensions: [".js", ".android.js", ".ios.js"]
-      }
-    }
-  }
+        extensions: [".js", ".android.js", ".ios.js"],
+      },
+    },
+  },
 };
