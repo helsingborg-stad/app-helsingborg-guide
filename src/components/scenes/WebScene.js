6562b66c6c776631598fb988493823d40da30508
Viktor Holtenäs <viktor@ustwo.com>
2017-11-02 19:49:53 +0100

    Cleaned up WebScene

--- src/components/scenes/WebScene.js
+++ src/components/scenes/WebScene.js
@@ -1,29 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import { WebView, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import ViewContainer from "../shared/view_container";
 import Navbar from "../shared/navbar";
 
-export default class TestScene extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export default (props) => {
+  const leftBtn = (
+    <TouchableOpacity style={{ flex: 1, alignItems: "center", justifyContent: "center" }} onPress={() => props.navigation.goBack()}>
+      <Icon name="chevron-left" size={20} color="white" />
+    </TouchableOpacity>
+  );
 
-  render() {
-    const leftBtn = (
-      <TouchableOpacity style={{ flex: 1, alignItems: "center", justifyContent: "center" }} onPress={() => this.props.navigator.pop()}>
-        <Icon name="chevron-left" size={20} color="white" />
-      </TouchableOpacity>
-    );
+  const { params } = props.navigation.state;
 
-    const { params } = this.props.navigation.state;
-
-    return (
-      <ViewContainer>
-        <Navbar title={this.props.text} leftButton={leftBtn} backgroundColor="#D35098" />
-        <WebView source={{ uri: params.url }} />
-      </ViewContainer>
-    );
-  }
-}
+  return (
+    <ViewContainer>
+      <Navbar title={props.text} leftButton={leftBtn} backgroundColor="#D35098" />
+      <WebView source={{ uri: params.url }} />
+    </ViewContainer>
+  );
+};
