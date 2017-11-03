050a4621011726ee2c6ac4227b6499726ead1d51
Viktor Holtenäs <viktor@ustwo.com>
2017-11-03 12:21:25 +0100

    Code cleanup

--- src/components/shared/MenuView.js
+++ src/components/shared/MenuView.js
@@ -1,14 +1,29 @@
-/**
- * Created by msaeed on 2017-02-04.
- */
 import React, { Component } from "react";
-import { View, Text, Image, StyleSheet, Animated, Dimensions } from "react-native";
+import { View, StyleSheet, Animated, Dimensions } from "react-native";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as menuActions from "../../actions/menuActions";
 
 const MENU_WIDTH = Dimensions.get("window").width * 0.9;
 const MENU_HEIGHT = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    backgroundColor: "white",
+    width: MENU_WIDTH,
+    height: MENU_HEIGHT,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    zIndex: 10000,
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+});
 
 class MenuView extends Component {
   constructor(props) {
@@ -19,20 +34,14 @@ class MenuView extends Component {
     };
   }
 
-  componentDidMount() {
-    this.configPosition();
-  }
-
   componentWillReceiveProps(nextProps) {
-    if (nextProps.visible != this.state.visible) {
+    if (nextProps.visible !== this.state.visible) {
       this.setState({
         visible: nextProps.visible,
       });
       this.animate(nextProps.visible);
     }
   }
-
-  configPosition() {}
 
   animate(visible) {
     if (visible) Animated.spring(this.state.animValue, { toValue: 1, tension: 60, friction: 6 }).start();
@@ -60,27 +69,8 @@ class MenuView extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  wrapper: {
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    backgroundColor: "white",
-    width: MENU_WIDTH,
-    height: MENU_HEIGHT,
-    position: "absolute",
-    top: 0,
-    right: 0,
-    zIndex: 10000,
-  },
-  mainContainer: {
-    flex: 1,
-    backgroundColor: "red",
-  },
-});
-
 // store config
-
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state) {
   return {
     visible: state.menu.visible,
   };
