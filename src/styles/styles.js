50f71fadfdc1dab54b86e8d5f8a1bc326ace5df4
Viktor Holtenäs <viktor@ustwo.com>
2017-11-03 10:02:00 +0100

    One less lint error

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,56 +4,53 @@ import { StyleSheet } from "react-native";
 // ######################################################
 // ######################################################
 
-export default StyleSheet.create(
-  (styles = {
-    mapViewContainer: { flex: 4, justifyContent: "flex-start", alignItems: "stretch" },
-    map: { flex: 1 },
-    openTimeContainer: { paddingVertical: 20 },
-    openTimeText: { fontSize: 16, fontWeight: "300", lineHeight: 19 },
+export default StyleSheet.create({
+  mapViewContainer: { flex: 4, justifyContent: "flex-start", alignItems: "stretch" },
+  map: { flex: 1 },
+  openTimeContainer: { paddingVertical: 20 },
+  openTimeText: { fontSize: 16, fontWeight: "300", lineHeight: 19 },
 
-    guideList: {
-      flex: 1,
-    },
+  guideList: {
+    flex: 1,
+  },
 
-    listViewContainer: {
-      flex: 3,
-    },
+  listViewContainer: {
+    flex: 3,
+  },
 
-    guideScroll: {
-      justifyContent: "flex-start",
-      alignItems: "center",
-      paddingHorizontal: 50,
-      backgroundColor: "#fefefe",
-    },
+  guideScroll: {
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingHorizontal: 50,
+    backgroundColor: "#fefefe",
+  },
 
-    titleContainer: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 1,
-      // backgroundColor:'blue',
-    },
-    title: { fontSize: 23, fontWeight: "bold", textAlign: "center" },
+  titleContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 1,
+  },
+  title: { fontSize: 23, fontWeight: "bold", textAlign: "center" },
 
-    navigateBtn: {
-      position: "absolute",
-      top: 6,
-      right: 6,
-      zIndex: 100,
-      width: 40,
-      height: 40,
-      backgroundColor: "#D35098",
-    },
+  navigateBtn: {
+    position: "absolute",
+    top: 6,
+    right: 6,
+    zIndex: 100,
+    width: 40,
+    height: 40,
+    backgroundColor: "#D35098",
+  },
 
-    guideView: {
-      height: 400,
-      flex: 1,
-      flexDirection: "column",
-    },
-    guideViewImage: {
-      height: 350,
-      resizeMode: "cover",
-      overflow: "hidden",
-    },
-  }),
-);
+  guideView: {
+    height: 400,
+    flex: 1,
+    flexDirection: "column",
+  },
+  guideViewImage: {
+    height: 350,
+    resizeMode: "cover",
+    overflow: "hidden",
+  },
+});
