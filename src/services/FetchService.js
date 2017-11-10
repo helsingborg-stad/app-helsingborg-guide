4955763f16c22c76dc8e35bd57e0ea677464d4cd
Christian Wiedel <christian@ustwo.com>
2017-11-10 16:14:37 +0100

    class methods that dont use this to static methods

--- src/services/FetchService.js
+++ src/services/FetchService.js
@@ -1,26 +1,70 @@
 /**
  * Created by msaeed on 2017-04-10.
  */
+
 import * as _ from "lodash";
 import RNFetchBlob from "react-native-fetch-blob";
-import { PermissionsAndroid, Platform } from "react-native";
+import {
+  PermissionsAndroid,
+  Platform,
+} from "react-native";
 
 let instance = null;
-
 const Headers = ["http://", "https://", "ftp://"];
-
 const MIME = ["jpeg", "jpg", "png", "mp3", "mp4", "m4a"];
 
-export class FetchService {
+export default class FetchService {
   basePath;
 
   constructor() {
     this.basePath = RNFetchBlob.fs.dirs.CacheDir;
   }
+
   static getInstance() {
     if (!instance) instance = new FetchService();
     return instance;
   }
+
+  static isString(data) {
+    return typeof data === "string";
+  }
+
+  static isObject(data) {
+    if (!data) return false;
+    return typeof data === "object";
+  }
+
+  static isUrl(str) {
+    if (!FetchService.isString(str)) return false;
+    const index = _.findIndex(Headers, header => _.startsWith(_.toLower(str), header));
+    return !(index === -1);
+  }
+
+  static isDownloadableUrl(str) {
+    if (!FetchService.isUrl(str)) return false;
+    const index = _.findIndex(MIME, ext => _.endsWith(_.toLower(str), `.${ext}`));
+    return !(index === -1);
+  }
+
+  static clearCache(path) {
+    return RNFetchBlob.fs.unlink(path);
+  }
+
+  static clearSessionCache(sessionId) {
+    return RNFetchBlob.session(sessionId).dispose();
+  }
+
+  static readFile(path) {
+    return RNFetchBlob.fs.readFile(path, "base64");
+  }
+
+  static getExt(url) {
+    const l = url.length;
+    if (!l) return "";
+    // what to do
+    return url[l - 3] + url[l - 2] + url[l - 1];
+  }
+
   // ######################################
   // The Json scanner
   scanJsonTree(mObject) {
@@ -29,44 +73,20 @@ export class FetchService {
 
     if (!keys.length) return paths;
 
-    keys.forEach((key, index) => {
-      // console.log(typeof mObject[key]);
-
+    keys.forEach((key) => {
       const item = mObject[key];
-      if (this._isDownloadableUrl(item)) {
+      if (FetchService.isDownloadableUrl(item)) {
         paths = paths.concat([item]);
       }
 
-      if (this._isObject(item)) paths = paths.concat(this.scanJsonTree(item));
+      if (FetchService.isObject(item)) paths = paths.concat(this.scanJsonTree(item));
     });
 
     return _.uniq(paths);
   }
 
-  _isString(data) {
-    return typeof data === "string";
-  }
-
-  _isObject(data) {
-    if (!data) return false;
-    return typeof data === "object";
-  }
-  _isUrl(str) {
-    if (!this._isString(str)) return false;
-    const index = _.findIndex(Headers, header => _.startsWith(_.toLower(str), header));
-    return !(index == -1);
-  }
-  _isDownloadableUrl(str) {
-    if (!this._isUrl(str)) return false;
-    const index = _.findIndex(MIME, ext => _.endsWith(_.toLower(str), `.${ext}`));
-    return !(index == -1);
-  }
   // ########################################
   fetch(url) {
-    // console.log(dirs.DocumentDir)
-    // console.log(dirs.CacheDir)
-    // console.log(dirs.DCIMDir)
-    // console.log(dirs.DownloadDir)
     const config = {
       fileCache: true,
       path: `${this.basePath}/${url}`,
@@ -84,31 +104,14 @@ export class FetchService {
     });
   }
 
-  clearCache(path) {
-    return RNFetchBlob.fs.unlink(path);
-  }
-
-  clearSessionCache(sessionId) {
-    return RNFetchBlob.session(sessionId).dispose();
-  }
-
-  readFile(path) {
-    return RNFetchBlob.fs.readFile(path, "base64");
-  }
-
   isExist(path) {
     const fullPath = this.getFullPath(path);
-    return RNFetchBlob.fs.exists(Platform.OS == "android" ? `file://${fullPath}` : fullPath);
+    return RNFetchBlob.fs.exists(Platform.OS === "android" ? `file://${fullPath}` : fullPath);
   }
+
   getFullPath(_path) {
     let path = _path;
     if (!path) path = "";
     return `${this.basePath}/${path}`;
-  }
-  getExt(url) {
-    const l = url.length;
-    if (!l) return "";
-    // what to do
-    return url[l - 3] + url[l - 2] + url[l - 1];
   }
 }
