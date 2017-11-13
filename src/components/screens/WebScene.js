cb127f8ba039e7fb69bac381e46d9ca197da4f02
Viktor Holtenäs <viktor@ustwo.com>
2017-11-13 13:48:27 +0100

    Removed old navbar from WebScene

--- src/components/screens/WebScene.js
+++ src/components/screens/WebScene.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { WebView, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { WebView, StyleSheet } from "react-native";
 import ViewContainer from "../shared/view_container";
-import Navbar from "../shared/navbar";
 
-export default (props) => {
-  const leftBtn = (
-    <TouchableOpacity style={{ flex: 1, alignItems: "center", justifyContent: "center" }} onPress={() => props.navigation.goBack()}>
-      <Icon name="chevron-left" size={20} color="white" />
-    </TouchableOpacity>
-  );
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#D35098",
+  },
+});
 
+const WebScene = (props) => {
   const { params } = props.navigation.state;
 
   return (
     <ViewContainer>
-      <Navbar title={props.text} leftButton={leftBtn} backgroundColor="#D35098" />
       <WebView source={{ uri: params.url }} />
     </ViewContainer>
   );
 };
+
+WebScene.navigationOptions = {
+  headerRight: null,
+  headerStyle: styles.header,
+};
+
+export default WebScene;
