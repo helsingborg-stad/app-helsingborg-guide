0b404f508dabeef0b622007c3fcc7a84c1379b38
Viktor Holtenäs <viktor@ustwo.com>
2017-11-20 08:16:55 +0100

    Cleaning up KeyPad

--- src/components/shared/KeyPad.js
+++ src/components/shared/KeyPad.js
@@ -1,22 +1,60 @@
-/**
- * Created by msaeed on 2017-02-04.
- */
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, Dimensions, TouchableOpacity, Animated } from "react-native";
-import ViewContainer from "../shared/view_container";
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity, Animated } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import RoundedBtn from "../shared/roundedBtn";
 import LangService from "../../services/langService";
 
-const NAVBAR_HEIGHT = 50;
 const FULL_HEIGHT = Dimensions.get("window").height;
 const FULL_WIDTH = Dimensions.get("window").width;
 const MAX_DIGITS = 3;
 const DEFAULT_CHAR = "_";
 const DEFAULT_ARR = [DEFAULT_CHAR, DEFAULT_CHAR, DEFAULT_CHAR];
 
-export default class Keypad extends Component {
-  resultCode;
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: "white",
+    height: FULL_HEIGHT,
+    width: FULL_WIDTH,
+    padding: 10,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    zIndex: 1000000,
+  },
+  headerContainer: {
+    height: 60,
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  btnContainer: { flex: 1, alignItems: "center", paddingHorizontal: 15 },
+  titleContainer: {
+    flex: 8,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    paddingHorizontal: 15,
+  },
+  titleText: { fontSize: 18, lineHeight: 30 },
+  padContainer: { flex: 1 },
+  rowContainer: { flex: 1, flexDirection: "row", justifyContent: "space-around" },
+  digitContainer: { backgroundColor: "#F0F0F0", alignItems: "center", justifyContent: "center", margin: 1 },
+  upperDigitContainer: { backgroundColor: "#F0F0F0", alignItems: "center", justifyContent: "center" },
+  rowItem: { flex: 1 },
+  doubleRowItem: { flex: 2 },
+  digitText: { fontSize: 48, lineHeight: 57, fontWeight: "300", color: "#999999" },
+  disabled: { backgroundColor: "#f8f8f8" },
+  disabledText: { color: "#d9d9d9" },
+});
+
+export default class KeyPad extends Component {
+  static strToArray(str, max) {
+    const arr = [];
+    for (let i = 0; i < max; i++) {
+      if (i >= str.length) arr.push(DEFAULT_CHAR);
+      else arr.push(str[i]);
+    }
+    return arr;
+  }
 
   constructor(props) {
     super(props);
@@ -34,23 +72,22 @@ export default class Keypad extends Component {
   componentDidMount() { }
 
   componentWillReceiveProps(nextProps) {
-    if (this.state.visible != nextProps.visible) {
+    if (this.state.visible !== nextProps.visible) {
       this.animate(nextProps.visible);
       this.setState({ visible: nextProps.visible });
     }
-    if (nextProps.resultCode != this.resultCode) {
-      console.log("has result is updated", nextProps.resultCode);
+    if (nextProps.resultCode !== this.resultCode) {
       this.clearAll();
       this.resultCode = nextProps.resultCode;
 
-      if (nextProps.resultCode == 404) {
+      if (nextProps.resultCode === 404) {
         this.shake();
       }
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.number.length == MAX_DIGITS) {
+  componentDidUpdate() {
+    if (this.state.number.length === MAX_DIGITS) {
       this.clearNumber();
       this.search();
     }
@@ -59,7 +96,6 @@ export default class Keypad extends Component {
   shake() {
     Animated.sequence([
       Animated.spring(this.state.shakeValue, { toValue: 1, velocity: 30 }),
-      // Animated.spring(this.state.shakeValue,{toValue:0,  velocity:70})
     ]).start();
   }
 
@@ -69,9 +105,9 @@ export default class Keypad extends Component {
   }
 
   onDigitPressed(digit) {
-    if (this.state.number.length == MAX_DIGITS) return;
+    if (this.state.number.length === MAX_DIGITS) return;
     const number = this.state.number + digit;
-    this.setState({ number, displayedNumber: this.strToArray(number, 3) });
+    this.setState({ number, displayedNumber: KeyPad.strToArray(number, 3) });
   }
   clearNumber() {
     this.setState({ number: "" });
@@ -86,15 +122,6 @@ export default class Keypad extends Component {
     this.props.onSearch(this.state.number);
   }
 
-  strToArray(str, max) {
-    const arr = [];
-    for (let i = 0; i < max; i++) {
-      if (i >= str.length) arr.push(DEFAULT_CHAR);
-      else arr.push(str[i]);
-    }
-    return arr;
-  }
-
   displayDigits() {
     return this.state.displayedNumber.map((digit, index) => (
       <View key={index} style={[styles.rowItem, styles.upperDigitContainer]}>
@@ -104,7 +131,7 @@ export default class Keypad extends Component {
   }
 
   render() {
-    const noNumber = this.state.displayedNumber == "";
+    const noNumber = this.state.displayedNumber === "";
 
     const translateY = this.state.animValue.interpolate({
       inputRange: [0, 1],
@@ -191,40 +218,3 @@ export default class Keypad extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  wrapper: {
-    backgroundColor: "white",
-    height: FULL_HEIGHT,
-    width: FULL_WIDTH,
-    padding: 10,
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    zIndex: 1000000,
-  },
-  headerContainer: {
-    height: 60,
-    backgroundColor: "white",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  btnContainer: { flex: 1, alignItems: "center", paddingHorizontal: 15 },
-  titleContainer: {
-    flex: 8,
-    alignItems: "flex-start",
-    justifyContent: "center",
-    // backgroundColor:'blue',
-    paddingHorizontal: 15,
-  },
-  titleText: { fontSize: 18, lineHeight: 30 },
-  padContainer: { flex: 1 },
-  rowContainer: { flex: 1, flexDirection: "row", justifyContent: "space-around" },
-  digitContainer: { backgroundColor: "#F0F0F0", alignItems: "center", justifyContent: "center", margin: 1 },
-  upperDigitContainer: { backgroundColor: "#F0F0F0", alignItems: "center", justifyContent: "center" },
-  rowItem: { flex: 1 },
-  doubleRowItem: { flex: 2 },
-  digitText: { fontSize: 48, lineHeight: 57, fontWeight: "300", color: "#999999" },
-  disabled: { backgroundColor: "#f8f8f8" },
-  disabledText: { color: "#d9d9d9" },
-});
