d0353c1eebaa79d43ede5fbd928f6fb63a9ad659
Christian Wiedel <christian@ustwo.com>
2017-11-22 17:34:02 +0100

    Remove GuideListStyles

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,4 +1,3 @@
 export { default as Colors } from "./Colors";
-export { default as GuideListStyles } from "./GuideListStyles";
 export { default as TabBarStyles } from "./TabBarStyles";
 export { default as TextStyles } from "./TextStyles";
