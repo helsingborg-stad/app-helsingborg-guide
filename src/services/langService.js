c4ac656b128971faee74d6153672ec821fe850c6
Anders Lystad Brevik <norwigi@ustwo.com>
2018-01-17 16:46:30 +0100

    iOS blank screen fix with languages

--- src/services/langService.js
+++ src/services/langService.js
@@ -15,11 +15,16 @@ export default class LangService {
   // TODO: All state from this class should be in the redux store
   static setLanguage(lang) {
     let code = DEFAULT_CODE;
-    code = lang;
+
+    if(strings[lang])
+    {
+      code = lang;
+    }
 
     LangService.strings = strings[code];
     LangService.instructions = instructions[code];
     LangService.code = code;
+
   }
 
   static storeLangCode(code) {
