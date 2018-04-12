02e5bcb646bfb1f5607105c68b999a14981b0cc9
Björn Persson <bjorn@ustwo.com>
2018-04-12 13:52:21 +0200

    flow in guideGroupReducer

--- src/reducers/guideGroupReducer.js
+++ src/reducers/guideGroupReducer.js
@@ -1,15 +1,17 @@
-import { FETCH_GUIDEGROUPS_FAILURE, FETCH_GUIDEGROUPS_REQUEST, FETCH_GUIDEGROUPS_SUCCESS } from "../actions/actionTypes";
+// @flow
+
+import type { Action, GuideGroup } from "../actions/actionTypes";
 import initialState from "./initialState";
 
-export default function guideGroupsReducer(state = initialState.guideGroups, action) {
+export default function guideGroupReducer(state: GuideGroup = initialState.guideGroups, action: Action): GuideGroup {
   switch (action.type) {
-    case FETCH_GUIDEGROUPS_REQUEST:
+    case "FETCH_GUIDEGROUPS_REQUEST":
       return { ...state, isFetching: true };
-    case FETCH_GUIDEGROUPS_SUCCESS: {
+    case "FETCH_GUIDEGROUPS_SUCCESS": {
       const items = action.guideGroups;
       return { ...state, items, isFetching: false };
     }
-    case FETCH_GUIDEGROUPS_FAILURE:
+    case "FETCH_GUIDEGROUPS_FAILURE":
       return { ...state, isFetching: false };
     default:
       return state;
