7132937949f3c89d84b2f2b08f53486a95b7871d
Björn Persson <bjorn@ustwo.com>
2018-05-03 17:01:35 +0200

    placeholder image added for svg icons with no uri

--- src/components/shared/SVGView.js
+++ src/components/shared/SVGView.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import SvgUri from "react-native-svg-uri";
 
+const placeholderImage = require("../../images/iconPointPropertyPlaceholder.svg");
 
 export default class SVGView extends Component {
   static displaySVG(logoType, placeholder, customStyle) {
-    if (!logoType) return <Text>{placeholder}</Text>;
-
     const styleObj = StyleSheet.flatten(customStyle);
+    const sourceImg = logoType ? { uri: logoType } : placeholderImage;
 
     return (
       <View style={customStyle}>
         <SvgUri
           width={styleObj.width}
           height={styleObj.height}
-          source={{ uri: logoType }}
+          // source={{ uri: logoType }}
+          // source={placeholderImage}
+          source={sourceImg}
         />
       </View>
     );
