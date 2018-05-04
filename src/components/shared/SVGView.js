2cc8395783fe3e90b42bccc8c90871a482cfc608
Björn Persson <bjorn@ustwo.com>
2018-05-04 11:04:06 +0200

    cleanup

--- src/components/shared/SVGView.js
+++ src/components/shared/SVGView.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import SvgUri from "react-native-svg-uri";
 
 type Props = {
