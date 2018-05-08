edda7cf1525e82d53eb1c94fcfe084c058c051af
Viktor Holtenäs <viktor@ustwo.com>
2018-05-08 12:47:28 +0200

    Render guide header image

--- src/components/shared/GuideView/index.js
+++ src/components/shared/GuideView/index.js
@@ -1,16 +1,20 @@
 // @flow
 
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import styles from "./styles";
 
 declare type Props = {
   guide: Guide
 }
 
-const GuideView = (props: Props) =>
-  (<View >
-    <Text>{`guide id: ${props.guide.id}`}</Text>
-  </View>);
+const GuideView = (props: Props) => {
+  const { guide } = props;
+  return (<ScrollView style={styles.container}>
+    <Image source={{ uri: guide.images.large }} style={styles.image} />
+    <Text>{guide.name}</Text>
+  </ScrollView>);
+};
 
 
 export default GuideView;
---
+++ src/components/shared/GuideView/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  image: {
+    aspectRatio: 18 / 9,
+    width: "100%",
+    height: "auto",
+  },
+});
+
+export default styles;
