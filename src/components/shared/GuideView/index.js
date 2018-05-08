b6c38e497a50ddb6accea36ccc6cfa2342bb9133
Viktor Holtenäs <viktor@ustwo.com>
2018-05-08 13:21:25 +0200

    Rendering content object images

--- src/components/shared/GuideView/index.js
+++ src/components/shared/GuideView/index.js
@@ -8,11 +8,36 @@ declare type Props = {
   guide: Guide
 }
 
+function renderContentObject(obj: any) {
+  // TODO use ContentObject type
+  const images: Images[] = obj.images;
+  // TODO return placeholder image
+  const uri = images.length > 0 ? images[0].medium : null;
+  return (
+    <View key={obj.id} style={styles.objectContainer}>
+      <Image
+        source={{ uri }}
+        style={styles.objectImage}
+        resizeMode="cover"
+      />
+    </View>
+  );
+}
+
+function renderContentObjects(contentObjects: any[]) {
+  // TODO use ContentObject type
+  return (<View style={styles.objectsContainer} >
+    {contentObjects.map(item => renderContentObject(item))}
+  </View>
+  );
+}
+
 const GuideView = (props: Props) => {
   const { guide } = props;
   return (<ScrollView style={styles.container}>
     <Image source={{ uri: guide.images.large }} style={styles.image} />
     <Text>{guide.name}</Text>
+    {renderContentObjects(guide.contentObjects)}
   </ScrollView>);
 };
 
--- src/components/shared/GuideView/styles.js
+++ src/components/shared/GuideView/styles.js
@@ -9,6 +9,20 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "auto",
   },
+  objectsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  objectContainer: {
+    width: "50%",
+    height: "auto",
+    padding: "4%",
+  },
+  objectImage: {
+    aspectRatio: 1,
+    width: "100%",
+    height: "auto",
+  },
 });
 
 export default styles;
