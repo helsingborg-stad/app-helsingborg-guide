ffbdfa3e17bf20ca36baa51a022254c37cadbae0
Viktor Holtenäs <viktor@ustwo.com>
2018-05-14 16:52:21 +0200

    Added test case

--- src/components/shared/GuideView/__tests__/index.test.js
+++ src/components/shared/GuideView/__tests__/index.test.js
@@ -8,14 +8,29 @@ import GuideView from "../index";
 
 import renderer from "react-test-renderer";
 
-const guide: Guide = require("./testdata/guide_no_tagline.json");
+const guideWithoutTagline: Guide = require("./testdata/guide_no_tagline.json");
+const guideWithTagline: Guide = require("./testdata/guide_tagline.json");
 
 test("Guide without tagline", () => {
   let i = 0;
   const now: Date = new Date("July 12, 2018 12:00:00");
   for (i = 0; i < 7; i += 1) {
     const tree = renderer.create(<GuideView
-      guide={guide}
+      guide={guideWithoutTagline}
+      onPressContentObject={() => { }}
+    />).toJSON();
+    expect(tree).toMatchSnapshot();
+
+    now.setDate(now.getDate() + 1);
+  }
+});
+
+test("Guide with tagline", () => {
+  let i = 0;
+  const now: Date = new Date("July 12, 2018 12:00:00");
+  for (i = 0; i < 7; i += 1) {
+    const tree = renderer.create(<GuideView
+      guide={guideWithTagline}
       onPressContentObject={() => { }}
     />).toJSON();
     expect(tree).toMatchSnapshot();
