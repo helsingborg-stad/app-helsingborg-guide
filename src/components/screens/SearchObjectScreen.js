9448c1cae6f1b3d1a0dcf58be94d009a71790e1d
Viktor Holtenäs <viktor@ustwo.com>
2018-05-15 10:37:43 +0200

    Replaced KeyPad custom intro animation with react-navigation default

---
+++ src/components/screens/SearchObjectScreen.js
@@ -0,0 +1,28 @@
+// @flow
+
+import React, { Component } from "react";
+import KeyPad from "../shared/KeyPad";
+
+type Props = {
+  navigation: any,
+}
+
+class SearchObjectScreen extends Component<Props> {
+  onPressClose = () => {
+    const { goBack } = this.props.navigation;
+    goBack();
+  }
+
+  onSearch = (id: string) => {
+    console.log(`onSearch: ${id}`);
+  }
+
+  render() {
+    return (<KeyPad onPressClose={this.onPressClose} onSearch={this.onSearch} />);
+  }
+}
+
+// TODO connect to redux store, in order to be able to search the current content objects
+
+
+export default SearchObjectScreen;
