38944d01470b80f1890cb8767105cff89c6e0437
Daniel Block <block@ustwo.com>
2018-05-30 10:34:00 +0200

    Fix imports of json schemas

--- src/utils/JSONValidator.js
+++ src/utils/JSONValidator.js
@@ -1,20 +1,21 @@
 // @flow
 
-import { Ajv } from "ajv";
+import Ajv from "ajv";
 
-import { jsonSchema } from "ajv/lib/refs/json-schema-draft-06.json";
-import { beacon } from "../../json-schemas/beacon.json";
-import { contentObject } from "../../json-schemas/contentObject.json";
-import { guide } from "../../json-schemas/guide.json";
-import { guideGroup } from "../../json-schemas/guideGroup.json";
-import { link } from "../../json-schemas/link.json";
-import { location } from "../../json-schemas/location.json";
-import { mediaContent } from "../../json-schemas/mediaContent.json";
-import { pointProperty } from "../../json-schemas/pointProperty.json";
-import { position } from "../../json-schemas/position.json";
+import jsonSchema from "ajv/lib/refs/json-schema-draft-06.json";
 
-const options: Ajv.Options = { verbose: true };
-const ajv = new Ajv(options);
+const beacon = require("../../json-schemas/beacon.json");
+const contentObject = require("../../json-schemas/contentObject.json");
+const guide = require("../../json-schemas/guide.json");
+const guideGroup = require("../../json-schemas/guideGroup.json");
+const link = require("../../json-schemas/link.json");
+const location = require("../../json-schemas/location.json");
+const mediaContent = require("../../json-schemas/mediaContent.json");
+const pointProperty = require("../../json-schemas/pointProperty.json");
+const position = require("../../json-schemas/position.json");
+
+const options = { verbose: true };
+const ajv = Ajv(options);
 
 ajv.addMetaSchema(jsonSchema);
 
