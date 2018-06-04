8d1806c95347109c3c6a0b3059e74199f4cb4e01
Daniel Block <block@ustwo.com>
2018-06-04 10:21:01 +0200

    Validate fetched guide data

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { API_BASE_URL } from "../data/repo/endpoints";
+import { validate } from "./JSONValidator";
 
 async function getGuideGroups(langCode: string): Promise<GuideGroup[]> {
   const url = `${API_BASE_URL}/guidegroup/?lang=${langCode}`;
@@ -18,14 +19,25 @@ async function getGuideGroups(langCode: string): Promise<GuideGroup[]> {
 async function getGuides(langCode: string): Promise<Guide[]> {
   const url = `${API_BASE_URL}/guide/?lang=${langCode}`;
   const response = await fetch(url);
+
   if (!response.ok) {
     throw new Error("Failed to fetch guides");
   }
 
   const json = await response.json();
-  // TODO validate data
+  const fetchedGuides: Guide[] = [];
 
-  return json;
+  json.forEach((element: any) => {
+    try {
+      if (validate(element, "guide")) {
+        fetchedGuides.push(element);
+      }
+    } catch (err) {
+      console.warn("Validation failed for fetched guide.", err);
+    }
+  });
+
+  return fetchedGuides;
 }
 
 export default { getGuideGroups, getGuides };
