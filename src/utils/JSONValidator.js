a7193296e1340bdd8317b6d0ebbf28c40b07d96c
Daniel Block <block@ustwo.com>
2018-06-04 10:17:58 +0200

    Update json schemes

--- src/utils/JSONValidator.js
+++ src/utils/JSONValidator.js
@@ -4,15 +4,17 @@ import Ajv from "ajv";
 
 import jsonSchema from "ajv/lib/refs/json-schema-draft-06.json";
 
-const beacon = require("../../json-schemas/beacon.json");
-const contentObject = require("../../json-schemas/contentObject.json");
-const guide = require("../../json-schemas/guide.json");
-const guideGroup = require("../../json-schemas/guideGroup.json");
-const link = require("../../json-schemas/link.json");
-const location = require("../../json-schemas/location.json");
-const mediaContent = require("../../json-schemas/mediaContent.json");
-const pointProperty = require("../../json-schemas/pointProperty.json");
-const position = require("../../json-schemas/position.json");
+const beacon = require("../../json-schemas/IBeacon.json");
+const contentObject = require("../../json-schemas/IContentObject.json");
+const guide = require("../../json-schemas/IGuide.json");
+const guideGroup = require("../../json-schemas/IGuideGroup.json");
+const link = require("../../json-schemas/ILink.json");
+const location = require("../../json-schemas/ILocation.json");
+const mediaContent = require("../../json-schemas/IMediaContent.json");
+const navigationCategory = require("../../json-schemas/INavigationCategory.json");
+const navigationItem = require("../../json-schemas/INavigationItem.json");
+const pointProperty = require("../../json-schemas/IPointProperty.json");
+const position = require("../../json-schemas/IPosition.json");
 
 const options = { verbose: true };
 const ajv = Ajv(options);
@@ -29,6 +31,8 @@ ajv.addSchema(link, "link");
 ajv.addSchema(beacon, "beacon");
 ajv.addSchema(position, "position");
 ajv.addSchema(location, "location");
+ajv.addSchema(navigationCategory, "navigationCategory");
+ajv.addSchema(navigationItem, "navigationItem");
 
 export function validate(data: any, schema: string): boolean {
   const result = ajv.validate(schema, data);
