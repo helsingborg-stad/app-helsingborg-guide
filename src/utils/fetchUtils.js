5cba8dac2369c3836a6d53ab122a468635e3cc8e
Daniel Block <block@ustwo.com>
2018-06-04 11:22:26 +0200

    Extract validation function in fetchUtils

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -3,6 +3,22 @@
 import { API_BASE_URL } from "../data/repo/endpoints";
 import { validate } from "./JSONValidator";
 
+function validateData(data: any, scheme: string): any[] {
+  const validatedData: any[] = [];
+
+  data.forEach((element: any) => {
+    try {
+      if (validate(element, scheme)) {
+        validatedData.push(element);
+      }
+    } catch (err) {
+      console.warn("Validation failed for fetched ", scheme, err);
+    }
+  });
+
+  return validatedData;
+}
+
 async function getGuideGroups(langCode: string): Promise<GuideGroup[]> {
   const url = `${API_BASE_URL}/guidegroup/?lang=${langCode}`;
   const response = await fetch(url);
@@ -11,17 +27,7 @@ async function getGuideGroups(langCode: string): Promise<GuideGroup[]> {
   }
 
   const json = await response.json();
-  const fetchedGuideGroups: GuideGroup[] = [];
-
-  json.forEach((element: any) => {
-    try {
-      if (validate(element, "guideGroup")) {
-        fetchedGuideGroups.push(element);
-      }
-    } catch (err) {
-      console.warn("Validation failed for fetched guide group.", err);
-    }
-  });
+  const fetchedGuideGroups: GuideGroup[] = validateData(json, "guideGroup");
 
   return fetchedGuideGroups;
 }
@@ -29,23 +35,12 @@ async function getGuideGroups(langCode: string): Promise<GuideGroup[]> {
 async function getGuides(langCode: string): Promise<Guide[]> {
   const url = `${API_BASE_URL}/guide/?lang=${langCode}`;
   const response = await fetch(url);
-
   if (!response.ok) {
     throw new Error("Failed to fetch guides");
   }
 
   const json = await response.json();
-  const fetchedGuides: Guide[] = [];
-
-  json.forEach((element: any) => {
-    try {
-      if (validate(element, "guide")) {
-        fetchedGuides.push(element);
-      }
-    } catch (err) {
-      console.warn("Validation failed for fetched guide.", err);
-    }
-  });
+  const fetchedGuides: Guide[] = validateData(json, "guide");
 
   return fetchedGuides;
 }
