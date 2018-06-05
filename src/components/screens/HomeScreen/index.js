cfd664c823618fbd45bb5fc76bc326ab21c4510c
Björn Persson <bjorn@ustwo.com>
2018-06-05 14:09:02 +0200

    merge

--- src/components/screens/HomeScreen/index.js
+++ src/components/screens/HomeScreen/index.js
@@ -157,10 +157,12 @@ class HomeScreen extends Component<Props> {
 function mapStateToProps(state: RootState) {
   const { navigation } = state;
   const { isFetching, navigationCategories } = navigation;
+  const { currentBottomBarTab } = state.uiState;
 
   return {
     showLoadingSpinner: isFetching,
     navigationSections: navigationCategories,
+    currentBottomBarTab,
   };
 }
 
