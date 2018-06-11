4c4a53ffe12f2cc86b32911e48f026f864a0d67f
Björn Persson <bjorn@ustwo.com>
2018-06-11 22:21:02 +0200

    ios ok

--- src/components/shared/BottomBarView/index.js
+++ src/components/shared/BottomBarView/index.js
@@ -3,7 +3,7 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
-import { Animated, View, Image } from "react-native";
+import { Animated, View, Image, Dimensions, Platform } from "react-native";
 import styles from "./style";
 import { selectCurrentBottomBarTab } from "../../../actions/uiStateActions";
 import BottomBarIcon from "../../../components/shared/BottomBarIcon";
@@ -17,6 +17,21 @@ const barTabRightDisabled = require("../../../images/bottom-right-disabled.png")
 const barTabCenterDisabled = require("../../../images/bottom-center-disabled.png");
 const barTabLeftDisabled = require("../../../images/bottom-left-disabled.png");
 
+// Sorry /Björn
+const isIphoneX = () => {
+  const d = Dimensions.get("window");
+  const { height, width } = d;
+
+  return (
+    Platform.OS === "ios" &&
+    (height === 812 || width === 812)
+  );
+};
+
+const viewContainerHeight: number = isIphoneX() ? 80 : 68;
+const buttonTabBottom: number = isIphoneX() ? 77 : 68;
+const transitionDuration: number = 300;
+
 type Props = {
   currentBottomBarTab: number,
   showBottomBar: boolean,
@@ -24,71 +39,83 @@ type Props = {
 }
 
 type State = {
-  animValue: Animated.Value
+  animViewContainer: Animated.Value,
+  animTabBottom: Animated.Value,
 }
-
-
-function displayButtonTabs(currentBottomBarTab: number) {
-  return (
-    <View style={styles.buttonTabContainer}>
-      <Image style={styles.imageTab} resizeMode="stretch" source={currentBottomBarTab === 0 ? barTabLeft : barTabLeftDisabled} />
-      <Image style={styles.imageTab} resizeMode="stretch" source={currentBottomBarTab === 1 ? barTabCenter : barTabCenterDisabled} />
-      <Image style={styles.imageTab} resizeMode="stretch" source={currentBottomBarTab === 2 ? barTabRight : barTabRightDisabled} />
-    </View>
-  );
-}
-
 
 class BottomBarView extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      animValue: new Animated.Value(100),
+      animViewContainer: new Animated.Value(0),
+      animTabBottom: new Animated.Value(0),
     };
   }
+
   componentDidMount() {
-    // Animated.timing(this.state.animValue, { toValue: 100, duration: 5000 }).start();
-    // console.log("start animate on mount");
   }
 
   componentWillReceiveProps(nextProps: Props) {
     if (nextProps.showBottomBar && !this.props.showBottomBar) {
-      console.log("start animate on will receive");
-      Animated.timing(this.state.animValue, { toValue: 0, duration: 1000 }).start();
+      Animated.timing(this.state.animViewContainer, { toValue: viewContainerHeight, duration: transitionDuration }).start();
+      Animated.timing(this.state.animTabBottom, { toValue: buttonTabBottom, duration: transitionDuration }).start();
     } else if (!nextProps.showBottomBar && this.props.showBottomBar) {
-      this.setState({ animValue: new Animated.Value(100) });
-      console.log("reset animate on will receive");
+      this.setState({ animViewContainer: new Animated.Value(0) });
+      this.setState({ animTabBottom: new Animated.Value(0) });
     }
   }
 
-  displayIcons(currentBottomBarTab: number) {
+  displayIcons() {
     return (
       <View style={styles.iconContainer}>
-        <BottomBarIcon index={0} selected={currentBottomBarTab === 0} selectBottomBarTab={this.props.selectBottomBarTab} />
-        <BottomBarIcon index={1} selected={currentBottomBarTab === 1} selectBottomBarTab={this.props.selectBottomBarTab} />
-        <BottomBarIcon index={2} selected={currentBottomBarTab === 2} selectBottomBarTab={this.props.selectBottomBarTab} />
+        <BottomBarIcon index={0} selected={this.props.currentBottomBarTab === 0} selectBottomBarTab={this.props.selectBottomBarTab} />
+        <BottomBarIcon index={1} selected={this.props.currentBottomBarTab === 1} selectBottomBarTab={this.props.selectBottomBarTab} />
+        <BottomBarIcon index={2} selected={this.props.currentBottomBarTab === 2} selectBottomBarTab={this.props.selectBottomBarTab} />
       </View >
     );
   }
+
+  displayButtonTabs() {
+    return (
+      <Animated.View style={[styles.buttonTabContainer,
+      { bottom: this.state.animTabBottom },
+      ]}
+      >
+        <Image
+          style={styles.imageTab}
+          resizeMode="stretch"
+          source={this.props.currentBottomBarTab === 0 ? barTabLeft : barTabLeftDisabled}
+        />
+        <Image
+          style={styles.imageTab}
+          resizeMode="stretch"
+          source={this.props.currentBottomBarTab === 1 ? barTabCenter : barTabCenterDisabled}
+        />
+        <Image
+          style={styles.imageTab}
+          resizeMode="stretch"
+          source={this.props.currentBottomBarTab === 2 ? barTabRight : barTabRightDisabled}
+        />
+      </Animated.View>
+    );
+  }
+
 
   render() {
     if (!this.props.showBottomBar) { return null; }
 
     return (
       <Animated.View style={[styles.viewContainer,
-      { transform: [{ translateY: this.state.animValue }] },
+      { height: this.state.animViewContainer },
       ]}
       >
-        {displayButtonTabs(this.props.currentBottomBarTab)}
+        {this.displayButtonTabs()}
         <Image
           style={styles.imageBackground}
           source={barBackground}
         />
-        {this.displayIcons(this.props.currentBottomBarTab)}
-
-
+        {this.displayIcons()}
       </Animated.View>
-
     );
   }
 }
@@ -109,3 +136,6 @@ function mapDispatchToProps(dispatch: Dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BottomBarView);
+
+
+{ /* <View style={[styles.buttonTabContainer, { bottom: this.state.animTabBottom }]}> */ }
