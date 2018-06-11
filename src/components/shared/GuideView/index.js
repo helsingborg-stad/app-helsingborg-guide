e7641384cd862909e23bd10cf6df984dd9f01022
Daniel Block <block@ustwo.com>
2018-06-11 14:53:01 +0200

    Add share button in GuideView

--- src/components/shared/GuideView/index.js
+++ src/components/shared/GuideView/index.js
@@ -15,11 +15,12 @@ import ImageView from "../ImageView";
 import DateView from "../DateView";
 import AudioPlayerView from "../AudioPlayerView";
 import DownloadButtonContainer from "../DownloadButton";
+import SharingService from "../../../services/SharingService";
 
 declare type Props = {
   guide: Guide,
-  onPressContentObject(obj: ContentObject): void,
-}
+  onPressContentObject(obj: ContentObject): void
+};
 
 const textMaxHeight = Dimensions.get("window").height * 0.2;
 
@@ -38,16 +39,24 @@ class GuideView extends Component<Props> {
           style={styles.objectImage}
           resizeMode="cover"
         />
-        <Text style={styles.contentIdText} numberOfLines={1}>#{obj.searchableId}</Text>
-        <Text style={styles.contentTitleText} numberOfLines={2}>{obj.title}</Text>
-      </TouchableOpacity >
+        <Text style={styles.contentIdText} numberOfLines={1}>
+          #{obj.searchableId}
+        </Text>
+        <Text style={styles.contentTitleText} numberOfLines={2}>
+          {obj.title}
+        </Text>
+      </TouchableOpacity>
     );
-  }
+  };
 
-  renderContentObjects = (sessionId: number, contentObjects: ContentObject[]) => (<View style={styles.objectsContainer} >
-    {contentObjects.map(item => this.renderContentObject(sessionId, item))}
-  </View>
-  )
+  renderContentObjects = (
+    sessionId: number,
+    contentObjects: ContentObject[],
+  ) => (
+    <View style={styles.objectsContainer}>
+      {contentObjects.map(item => this.renderContentObject(sessionId, item))}
+    </View>
+  );
 
   render() {
     const { guide } = this.props;
@@ -55,25 +64,37 @@ class GuideView extends Component<Props> {
     return (
       <View style={styles.viewContainer}>
         <ScrollView style={styles.container}>
-          <ImageView source={{ uri: guide.images.large, sessionId: id }} style={styles.image} />
-          <DownloadButtonContainer
-            style={styles.downloadButton}
-          />
+          <View>
+            <ImageView
+              source={{ uri: guide.images.large, sessionId: id }}
+              style={styles.image}
+            />
+            <View style={styles.shareBtn}>
+              {SharingService.showShareButton(guide.name, guide.images, this)}
+            </View>
+          </View>
+          <DownloadButtonContainer style={styles.downloadButton} />
           <View style={styles.textContainer}>
-            <Text style={styles.title} numberOfLines={1}>{guide.name}</Text>
+            <Text style={styles.title} numberOfLines={1}>
+              {guide.name}
+            </Text>
             <View style={styles.optionalTexts}>
-              {guide.tagline ? <Text style={styles.guideTaglineText}>{guide.tagline}</Text> : null}
+              {guide.tagline ? (
+                <Text style={styles.guideTaglineText}>{guide.tagline}</Text>
+              ) : null}
               <DateView startDate={guide.dateStart} endDate={guide.dateEnd} />
             </View>
-            {guide.description ?
+            {guide.description ? (
               <ExpandableView maxHeight={textMaxHeight}>
                 <Text style={TextStyles.body}>{guide.description}</Text>
-              </ExpandableView> : null}
+              </ExpandableView>
+            ) : null}
           </View>
           {this.renderContentObjects(id, guide.contentObjects)}
         </ScrollView>
         <AudioPlayerView />
-      </View>);
+      </View>
+    );
   }
 }
 
