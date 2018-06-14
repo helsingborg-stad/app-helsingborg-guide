6d7ad1dada00a21780d089e0adb6faf8e223c3e7
Daniel Block <block@ustwo.com>
2018-06-14 15:05:18 +0200

    Open links from calendar in browser

--- src/components/screens/WebScreen.js
+++ src/components/screens/WebScreen.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { WebView, StyleSheet } from "react-native";
+import { WebView, StyleSheet, Linking } from "react-native";
 import PropTypes from "prop-types";
-import {
-  Colors,
-} from "../../styles/";
-
+import { Colors } from "../../styles/";
 
 const styles = StyleSheet.create({
   header: {
@@ -16,8 +13,18 @@ const WebScreen = (props) => {
   const { url } = props.navigation.state.params;
 
   return (
-    <WebView source={{ uri: url }} />
-
+    <WebView
+      ref={(ref) => {
+        this.webView = ref;
+      }}
+      source={{ uri: url }}
+      onNavigationStateChange={(event) => {
+        if (event.url !== url) {
+          this.webView.stopLoading();
+          Linking.openURL(event.url);
+        }
+      }}
+    />
   );
 };
 
