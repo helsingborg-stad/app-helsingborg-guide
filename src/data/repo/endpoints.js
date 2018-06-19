f984734fb96a1890b430d9a1cf9105f86d8fc7fd
Viktor Holtenäs <viktor@ustwo.com>
2018-06-19 09:59:44 +0200

    Removed all links to the old api

--- src/data/repo/endpoints.js
+++ src/data/repo/endpoints.js
@@ -1,3 +1,2 @@
-export const _API_BASE = "https://api.helsingborg.se/event/json/wp/v2";
 export const API_BASE_URL = __DEV__ ? "https://hbg-guide.herokuapp.com" : "https://guide-api.helsingborg.se/";
 export const _LANGUAGE_API_URL = `${API_BASE_URL}/languages`;
