d8d7d8931b8d350424519baf50e09ebaf13f5b1f
Erik Alfredsson <erikalfredsson@ustwo.com>
2019-05-17 10:42:27 +0200

    Use development middle layer api for debug builds only

--- src/data/repo/endpoints.js
+++ src/data/repo/endpoints.js
@@ -1,6 +1,4 @@
 import { NativeModules } from "react-native";
 
-const { applicationId } = NativeModules.Configuration;
-
-export const API_BASE_URL = applicationId === "com.ustwo.GuideHbg" ? "https://hbg-guide.herokuapp.com" : "https://guide-api.helsingborg.se/";
+export const API_BASE_URL = __DEV__ ? "https://hbg-guide.herokuapp.com" : "https://guide-api.helsingborg.se/";
 export const _LANGUAGE_API_URL = `${API_BASE_URL}/languages`;
