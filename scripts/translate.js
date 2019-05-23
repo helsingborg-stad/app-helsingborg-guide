98004bdcd184c404f039a7c015b42795d2b8f461
Matt O'Sullivan <mos@ustwo.com>
2019-05-23 17:02:00 +0200

    adding a script to convert translations (as .tsv files downloaded from Google Sheets) to the same object format used by the app

---
+++ scripts/translate.js
@@ -0,0 +1,93 @@
+const fs = require("fs");
+
+if (process.argv.length === 3) {
+  return console.error("Please pass a .tsv translations file and an output filename");
+}
+
+const INPUT_PATH = process.argv[2];
+const OUTPUT_PATH = process.argv[3];
+const SEPARATOR = "\t";
+const ENCODING = "utf8";
+const STRIP_REGEX = /[\r"]/gi;
+
+const indent = (strings, depth) => {
+  for (let i = 0; i < (2 * depth); i += 1) {
+    strings.push(" ");
+  }
+};
+
+const printObject = (object, strings, depth = 0) => {
+  const entries = Object.entries(object);
+
+  entries.forEach(([key, value]) => {
+    indent(strings, depth);
+    strings.push(key.replace(STRIP_REGEX, "").trim());
+    strings.push(": ");
+    if (typeof value === "object") {
+      strings.push("{\n");
+      printObject(value, strings, depth + 1);
+      indent(strings, depth);
+      strings.push("},\n");
+    } else {
+      strings.push("\"");
+      strings.push(value.replace(STRIP_REGEX, ""));
+      strings.push("\",\n");
+    }
+  });
+};
+
+const printHeader = () => [
+  "/**\n",
+  " * Created at ", new Date().toString(), "\n",
+  " */\n",
+  "\n",
+].join("");
+
+const printContent = (output) => {
+  const outputStrings = [];
+  printObject(output, outputStrings, 1);
+  return outputStrings.join("");
+};
+
+const readFile = filename => new Promise((resolve, reject) => {
+  fs.readFile(filename, ENCODING, (error, contents) => {
+    if (error) return reject(error);
+
+    const lines = contents.split("\n");
+    const headers = lines[0].split(SEPARATOR);
+
+    const object = lines.slice(1).reduce((output, line) => {
+      const translations = line.split(SEPARATOR);
+      const key = translations[0];
+
+      translations.forEach((translation, index) => {
+        if (index > 0) {
+          const header = headers[index];
+          if (!output[header]) output[header] = {};
+          output[header][key] = translation;
+        }
+      });
+
+      return output;
+    }, {});
+
+    const data = [
+      printHeader(),
+      "export default {\n",
+      printContent(object),
+      "};\n",
+    ].join("");
+
+    return resolve(data);
+  });
+});
+
+const writeFile = (filename, data) => new Promise((resolve, reject) => {
+  fs.writeFile(filename, data, error => (error ? reject(error) : resolve("Success!")));
+});
+
+(async function translate() {
+  const data = await readFile(INPUT_PATH);
+  const result = await writeFile(OUTPUT_PATH, data);
+  console.log(result);
+}());
