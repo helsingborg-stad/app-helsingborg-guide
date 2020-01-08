a8e9ee1ee1538b4d0194bd11c8fa70bf3b0ba563
joenas <jonnev@ustwo.com>
2020-01-08 14:08:52 +0100

    Removed /src in Lund

--- cities/Lund/src/actions/errorActions.js
+++
@@ -1,9 +0,0 @@
-import * as types from "./actionTypes";
-
-export function errorHappened(error) {
-  return { type: types.ERROR_HAPPENED, error };
-}
-
-export function clearError() {
-  return { type: types.CLEAR_ERROR };
-}
--- cities/Lund/src/components/shared/ImageSwiper/style.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export default StyleSheet.create({
-  imagesSlider: {},
-  image: {
-    width: "100%",
-    height: "100%"
-  }
-});
