6871797eaecc0306798f63007397a1de75bf6490
joenas <jonnev@ustwo.com>
2020-01-15 22:21:09 +0100

    Removing Helsingborg/iods/Pods

--- cities/Helsingborg/ios/Pods/Target Support Files/RNImageMarker/RNImageMarker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RNImageMarker : NSObject
-@end
-@implementation PodsDummy_RNImageMarker
-@end
--- cities/Helsingborg/ios/Pods/Target Support Files/react-native-simple-compass/react-native-simple-compass-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_react_native_simple_compass : NSObject
-@end
-@implementation PodsDummy_react_native_simple_compass
-@end
