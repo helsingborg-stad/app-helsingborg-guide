bd80dc429ae1cb93124a4142d5f6cbe3b6621c4a
joenas <jonnev@ustwo.com>
2020-01-16 09:04:06 +0100

    Removed Pods dirs, partly to be able to look at diff

--- cities/Helsingborg/ios/Pods/Headers/Private/RNImageMarker/RCTImageMarker.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-image-marker/ios/RCTImageMarker/RCTImageMarker.h
--- cities/Helsingborg/ios/Pods/Headers/Private/react-native-simple-compass/RNSimpleCompass.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-simple-compass/ios/RNSimpleCompass.h
