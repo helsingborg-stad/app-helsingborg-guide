2e0a1beeb227a2f8dc177a32d184155c33458675
joenas <jonnev@ustwo.com>
2020-01-16 15:45:06 +0100

    Moved tests/flow/eslint to guide-app and updated Travis build

---
+++ guide-app/babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        alias: {
+          "@actions": "./src/actions",
+          "@assets": "./assets/Helsingborg/assets",
+          "@data": "../cities/Helsingborg/data",
+          "@json-schemas": "./json-schemas",
+          "@services": "./src/services",
+          "@shared-components": "./src/components/shared",
+          "@src": "./src",
+          "@utils": "./src/utils"
+        }
+      }
+    ]
+  ]
+};
---
+++ guide-app/scripts/ci-build-node.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e -u
+
+NPM_BIN="$(npm bin)"
+ESLINT="$NPM_BIN/eslint"
+
+if hash gfind 2>/dev/null; then
+	FIND=gfind
+else
+	FIND=find
+fi
+
+{
+set -x
+
+yarn flow
+
+echo "TODO: enable eslint"
+# yarn eslint
+
+yarn test
+
+mkdir -p build/
+
+# Imitating node_modules/react-native/packager/react-native-xcode.sh
+react-native bundle --entry-file index.js --platform ios --dev false --reset-cache --bundle-output build/ios-release.jsbundle --assets-dest build/ios-assets-release
+
+# Imitating node_modules/react-native/react.gradle
+react-native bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output build/android-release.jsbundle --assets-dest build/android-assets-release
+
+}
+
+echo Finised all tests.
