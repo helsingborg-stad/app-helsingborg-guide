35db2352c481e2fa79785bf0e3d35bb534f3720e
joenas <jonnev@ustwo.com>
2020-01-17 09:31:06 +0100

    Updates to Travis: Run flow and tests in guide-app Build bundle in each city

--- cities/Helsingborg/scripts/ci-build-node.sh
+++ cities/Helsingborg/scripts/ci-build-node.sh
@@ -13,20 +13,13 @@ fi
 {
 set -x
 
-npm run-script flow
-
-echo "TODO: enable eslint"
-# npm run-script eslint
-
-npm run-script test
-
 mkdir -p build/
 
 # Imitating node_modules/react-native/packager/react-native-xcode.sh
-react-native bundle --entry-file index.js --platform ios --dev false --reset-cache --bundle-output build/ios-release.jsbundle --assets-dest build/ios-assets-release
+yarn react-native bundle --entry-file index.js --platform ios --dev false --reset-cache --bundle-output build/ios-release.jsbundle --assets-dest build/ios-assets-release
 
 # Imitating node_modules/react-native/react.gradle
-react-native bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output build/android-release.jsbundle --assets-dest build/android-assets-release
+yarn react-native bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output build/android-release.jsbundle --assets-dest build/android-assets-release
 
 }
 
