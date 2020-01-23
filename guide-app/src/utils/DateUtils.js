26383bf0390b0d139dc97f90d82c886fa4ee6142
joenas <jonnev@ustwo.com>
2020-01-23 13:45:17 +0100

    Added date-fns and DateUtils

---
+++ guide-app/src/utils/DateUtils.js
@@ -0,0 +1,20 @@
+// @flow
+import { de, en, sv } from "date-fns/locale";
+import { format } from "date-fns";
+
+const locales = { de, en, sv };
+
+// Dates from the API is always UTC
+function longDate(date: Date, locale: string): string {
+  return format(new Date(date), "PP", { locale: locales[locale] });
+}
+
+// https://date-fns.org/v2.9.0/docs/I18n
+function getHours(date: string, locale: string): string {
+  return format(new Date(date), "kk:mm", { locale: locales[locale] });
+}
+
+export default {
+  getHours,
+  longDate
+};
--- guide-app/src/utils/index.js
+++ guide-app/src/utils/index.js
@@ -1,4 +1,5 @@
 export { default as AnalyticsUtils } from "./AnalyticsUtils";
+export { default as DateUtils } from "./DateUtils";
 export { default as FetchUtils } from "./fetchUtils";
 export { default as StyleSheetUtils } from "./StyleSheetUtils";
 export { default as LocationUtils } from "./LocationUtils";
