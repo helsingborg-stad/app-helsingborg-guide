d65c895910dd8dca80af1f1eea7f9f2a3c6a0e03
Jonas Bengtsson <jonasb@ustwo.com>
2020-05-15 15:05:01 +0200

    Do not retain quiz state in async storage

--- guide-app/src/store/configureStore.js
+++ guide-app/src/store/configureStore.js
@@ -12,7 +12,7 @@ import patchContentMiddleware from "@src/middleware/patchContentMiddleware";
 const config = {
   key: "GuideAppRoot",
   storage: AsyncStorage,
-  blacklist: ["error", "menu", "internet", "audio", "uiState"],
+  blacklist: ["error", "menu", "internet", "audio", "uiState", "quiz"],
   version: 1,
   debug: __DEV__
 };
