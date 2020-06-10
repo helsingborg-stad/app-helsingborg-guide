bec3e853afe14a7c316d2c6dae82bfc203023f05
Henrik Persson <henrik@ustwo.com>
2020-06-10 19:49:36 +0200

    Remove root data folder

--- data/Lund/data/urls.js
+++
@@ -1,4 +0,0 @@
-export const eventCalendarURL = "https://kalender.helsingborg.se/event/?simpleAppView";
-
-export const sharingFadeUrl = "https://api.helsingborg.se/assets/guide/images/share_fade.png";
-export const sharingIconUrl = "https://api.helsingborg.se/assets/guide/images/share_icon.png";
