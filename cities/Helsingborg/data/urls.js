960a2ab36ceaa251f8944324dddd3481a3b3418b
Jonas Skovmand <worldeggplant@users.noreply.github.com>
2021-07-28 14:52:29 +0200

    Add navigation tab item for Zipadoo webshop

--- cities/Helsingborg/data/urls.js
+++ cities/Helsingborg/data/urls.js
@@ -4,3 +4,5 @@ export const sharingFadeUrl =
   "https://api.helsingborg.se/assets/guide/images/share_fade.png";
 export const sharingIconUrl =
   "https://api.helsingborg.se/assets/guide/images/share_icon.png";
+
+export const webShopUrl = "https://helsingborg.zipadoo.se/?ref=app";
