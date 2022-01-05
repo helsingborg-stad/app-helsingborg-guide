9febacea66dac82e0b1aa27a23c9dd003b78da18
Oskar Hertzman <oskar@ECs-MacBook-Pro.local>
2022-01-05 16:09:17 +0100

    added react-native-inappbrowser-reborn for in app url viewing

--- guide-app/src/components/shared/CalendarEvent.js
+++ guide-app/src/components/shared/CalendarEvent.js
@@ -6,8 +6,10 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
+  Alert
 } from "react-native";
+import { InAppBrowser } from "react-native-inappbrowser-reborn";
 import { decode } from "html-entities";
 
 import { Colors, TextStyles } from "@assets/styles";
@@ -131,10 +133,51 @@ function CalendarEvent({ event, currentLanguage }: Props) {
   const eventLinkDate = DateUtils.eventLinkDate(dateStart);
   const eventUrl = `${eventCalendarURL}/${slug}?date=${eventLinkDate}`;
 
+  const openLink = async (url) => {
+    try {
+      if (await InAppBrowser.isAvailable()) {
+        const result = await InAppBrowser.open(url, {
+          // iOS Properties
+          dismissButtonStyle: 'cancel',
+          preferredBarTintColor: '#701A5A',
+          preferredControlTintColor: 'white',
+          readerMode: false,
+          animated: true,
+          modalPresentationStyle: 'fullScreen',
+          modalTransitionStyle: 'coverVertical',
+          modalEnabled: true,
+          enableBarCollapsing: false,
+          // Android Properties
+          showTitle: true,
+          toolbarColor: '#701A5A',
+          secondaryToolbarColor: 'black',
+          navigationBarColor: 'black',
+          navigationBarDividerColor: 'white',
+          enableUrlBarHiding: true,
+          enableDefaultShare: true,
+          forceCloseOnRedirection: false,
+          // Specify full animation resource identifier(package:anim/name)
+          // or only resource name(in case of animation bundled with app).
+          animations: {
+            startEnter: 'slide_in_right',
+            startExit: 'slide_out_left',
+            endEnter: 'slide_in_left',
+            endExit: 'slide_out_right'
+          },
+          headers: {
+            'my-custom-header': 'my custom header value'
+          }
+        })
+      }
+      else Linking.openURL(url)
+    } catch (error) {
+      Alert.alert(error.message)
+    }
+  }
   return (
     <TouchableOpacity
       style={styles.item}
-      onPress={() => Linking.openURL(eventUrl)}
+      onPress={() => openLink(eventUrl)}
     >
       <View style={styles.imageWrapper}>
         <Image style={styles.listItemImage} source={image} />
