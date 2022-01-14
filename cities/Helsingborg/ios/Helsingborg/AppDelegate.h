8eac6ec89adc6e9f456d9868bcdd947505ecd28b
Oskar Hertzman <oskar.hertzman@hotmail.com>
2022-01-14 10:04:14 +0100

    fixed appDelegate import

--- cities/Helsingborg/ios/Helsingborg/AppDelegate.h
+++ cities/Helsingborg/ios/Helsingborg/AppDelegate.h
@@ -7,7 +7,7 @@
 
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
-import <UserNotifications/UNUserNotificationCenter.h>
+#import <UserNotifications/UNUserNotificationCenter.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
 
