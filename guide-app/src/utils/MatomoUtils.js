dbb5320f93a35fbfe4d897ccded0b93781125e10
Oskar Hertzman <oskar.hertzman@hotmail.com>
2022-01-24 08:39:01 +0100

    added fallback for non existing matomo values

--- guide-app/src/utils/MatomoUtils.js
+++ guide-app/src/utils/MatomoUtils.js
@@ -4,27 +4,31 @@ import { MATOMO_URL, MATOMO_SITE_ID } from "@data/endpoints";
 
 const isEmulator = async (func) => {
   DeviceInfo.isEmulator().then(device => {
-    device && func()
-  })
-}
+    device && func();
+  });
+};
 
 export const initializeTracker = () => {
-  isEmulator(Matomo.initTracker((MATOMO_URL.toString() + 'matomo.php' ), parseInt(MATOMO_SITE_ID, 10)))
-}
+  isEmulator(Matomo.initTracker((MATOMO_URL.toString() + "matomo.php"), parseInt(MATOMO_SITE_ID, 10)));
+};
 
 export const trackScreen = (path, title) => {
-  console.log("track", path, title)
-  isEmulator(Matomo.trackScreen(path, title))
-}
+  path = path ? path : "";
+  title = title ? title : "";
+  isEmulator(Matomo.trackScreen(path, title));
+};
 
 export const trackEvent = (category, action, name, value, url) => {
   name = name ? name : "";
   value = value ? value : "";
   url = url ? url : "";
 
-  isEmulator(Matomo.trackEvent(category, action, name, value, url))
-}
+  isEmulator(Matomo.trackEvent(category, action, name, value, url));
+};
 
 export const trackGoal = (goalID, revenue) => {
-    isEmulator(Matomo.trackGoal(goalID, revenue))
-}
+  goalID  = goalID ? goalID : "";
+  revenue = revenue ? revenue : "";
+  isEmulator(Matomo.trackGoal(goalID, revenue));
+
+};
