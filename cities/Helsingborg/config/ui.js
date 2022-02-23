2baade46e23ef23dce66ba25fac86c8c3e5fe8a8
Oskar Hertzman <oskar.hertzman@hotmail.com>
2022-02-23 11:20:18 +0100

    enabled scanner

--- cities/Helsingborg/config/ui.js
+++ cities/Helsingborg/config/ui.js
@@ -1,14 +1,14 @@
 import HomeButton from "@shared-components/BottomBarButton/buttons/HomeButton";
 import CalendarButton from "@shared-components/BottomBarButton/buttons/CalendarButton";
 import SettingsButton from "@shared-components/BottomBarButton/buttons/SettingsButton";
-// import QRButton from "@shared-components/BottomBarButton/buttons/QRButton";
+import QRButton from "@shared-components/BottomBarButton/buttons/QRButton";
 
 export const Navigation = {
   bottomBarButtons: [
     { id: "home", ButtonComponent: HomeButton },
     { id: "calendar", ButtonComponent: CalendarButton },
     { id: "settings", ButtonComponent: SettingsButton },
-    // { id: "qr", ButtonComponent: QRButton },
+    { id: "qr", ButtonComponent: QRButton },
   ],
 };
 
